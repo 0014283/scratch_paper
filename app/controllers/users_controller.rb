1d60c354398fab899f290fe443a6cf12ede0aa48
tsubasa ooishi <pop331240283@icloud.com>
2018-09-15 17:54:00 +0900

    Modify:paperの投稿一覧、新規投稿、詳細ページのビューを変更、ヘッダーのビューを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,4 @@
 class UsersController < ApplicationController
-	before_action :request_path
-
-
-	def request_path
-		@path = controller_path + '#' + action_name
-		def @path.is(*str)
-			str.map{|s| self.include?(s)}.include?(true)
-		end
-	end
 
 	def my_list
 		@user= User.find(params[:id])
